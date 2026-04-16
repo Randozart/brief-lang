f24663ad99205a024a9d0af379f85c30d0dc190e
Randozart <randozart@gmail.com>
2026-04-16 11:33:08 +0200

    Fix LSP launch for Flatpak by resolving absolute binary path
    - Updated extension.js to look for 'brief' in common locations (~/.local/bin, etc.)
    - Re-packaged and re-installed extension in VSCodium Flatpak

--- syntax-highlighter/client/extension.js
+++ syntax-highlighter/client/extension.js
@@ -1,15 +1,41 @@
 const vscode = require('vscode');
 const { LanguageClient, TransportKind } = require('vscode-languageclient/node');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 let client;
+
+function findBrief() {
+    // 1. Try common locations
+    const home = os.homedir();
+    const commonPaths = [
+        path.join(home, '.local/bin/brief'),
+        path.join(home, 'bin/brief'),
+        '/usr/local/bin/brief',
+        '/usr/bin/brief'
+    ];
+
+    for (const p of commonPaths) {
+        if (fs.existsSync(p)) {
+            return p;
+        }
+    }
+
+    // 2. Try PATH as a last resort
+    return 'brief';
+}
 
 function activate(context) {
     console.log('Brief extension activated');
 
+    const briefPath = findBrief();
+    console.log(`Using Brief binary at: ${briefPath}`);
+
     // The server is implemented in the brief binary
     const serverOptions = {
-        run: { command: 'brief', args: ['lsp'], transport: TransportKind.stdio },
-        debug: { command: 'brief', args: ['lsp'], transport: TransportKind.stdio }
+        run: { command: briefPath, args: ['lsp'], transport: TransportKind.stdio },
+        debug: { command: briefPath, args: ['lsp'], transport: TransportKind.stdio }
     };
 
     // Options to control the language client
